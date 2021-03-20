8cf77b15b8dfcf1ab246167ac9b990e30bc78ca2
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-03-20 12:39:26 -0700

    Use new fuse API

--- main.go
+++ main.go
@@ -1,30 +1,37 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
+	"syscall"
 
+	"github.com/hanwen/go-fuse/v2/fs"
 	"github.com/hanwen/go-fuse/v2/fuse"
 )
 
 type DBFS struct {
-	fuse.RawFileSystem
+	fs.Inode
 }
 
-func NewDBFS() *DBFS {
-	return &DBFS{
-		RawFileSystem: fuse.NewDefaultRawFileSystem(),
-	}
+func (r *DBFS) OnAdd(ctx context.Context) {
+	ch := r.NewPersistentInode(
+		ctx, &fs.MemRegularFile{
+			Data: []byte("file.txt"),
+			Attr: fuse.Attr{
+				Mode: 0644,
+			},
+		}, fs.StableAttr{Ino: 2})
+	r.AddChild("file.txt", ch, false)
 }
 
-func (fs *DBFS) GetAttr(cancel <-chan struct{}, input *fuse.GetAttrIn, out *fuse.AttrOut) (code fuse.Status) {
-	log.Printf("GetAttr happened: %+v\n", input)
-	return fuse.ENOSYS
+func (r *DBFS) Getattr(ctx context.Context, fh fs.FileHandle, out *fuse.AttrOut) syscall.Errno {
+	out.Mode = 0755
+	return 0
 }
 
-func (fs *DBFS) ReadDir(cancel <-chan struct{}, input *fuse.ReadIn, out *fuse.DirEntryList) fuse.Status {
-	return fuse.ENOSYS
-}
+var _ = (fs.NodeGetattrer)((*DBFS)(nil))
+var _ = (fs.NodeOnAdder)((*DBFS)(nil))
 
 func main() {
 	debug := flag.Bool("debug", false, "print debug data")
@@ -32,15 +39,17 @@ func main() {
 	if len(flag.Args()) < 1 {
 		log.Fatal("Usage:\n  hello MOUNTPOINT")
 	}
-	opts := &fuse.MountOptions{}
-	opts.Debug = *debug
-
-	fs := NewDBFS()
 	mountpoint := flag.Arg(0)
 
-	server, err := fuse.NewServer(fs, mountpoint, opts)
+	opts := &fs.Options{}
+	opts.Debug = *debug
+
+	if *debug {
+		log.Printf("Mounting on %s\n", mountpoint)
+	}
+	server, err := fs.Mount(mountpoint, &DBFS{}, opts)
 	if err != nil {
 		log.Fatalf("Mount fail: %v\n", err)
 	}
-	server.Serve()
+	server.Wait()
 }
